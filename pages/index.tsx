b0a544305b5454c8d859945dcbd09ca9c07bfb51
Saphal Patro <saphal1998@gmail.com>
2020-12-26 15:48:06 +0530

    Added scaffolding

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,11 +9,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        SEO Enhnacer init
-      </main>
-
-    
+      <main className={styles.main}>SEO Enhnacer init</main>
     </div>
   )
 }
