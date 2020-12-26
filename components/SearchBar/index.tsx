0b46fd73e964f92a9d4245b286ff6d0d5786eb01
Sarthak Kundra <sarthakkundra21@gmail.com>
2020-12-27 03:19:03 +0530

    made rough homepage

---
+++ components/SearchBar/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Input } from "@chakra-ui/react";
+
+const index = () => {
+    return (
+        <div>
+            
+            <Input focusBorderColor="#00dfd8" variant="outline" placeholder="www.example.com" /> 
+            
+        </div>
+    )
+}
+
+export default index
