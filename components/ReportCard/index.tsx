6af0098e922625ea412994210d68b09bc2c21118
Sarthak Kundra <sarthakkundra21@gmail.com>
2020-12-30 12:24:04 +0530

    UI improvements on report

--- components/ReportCard/index.tsx
+++ components/ReportCard/index.tsx
@@ -8,16 +8,20 @@ import styles from './index.module.css';
 const index = () => {
     return (
         <div className={clsx(styles.box)}>
+             <span className={clsx(styles.tickAlign)}>
+          
+          <Success />
+          </span>
+
             <span className={clsx(styles.headingAlign)}>
               Test stat
               </span>
-              <span className={clsx(styles.contentAlign)}>
-                      <p>sddikffbhsdjkdfvbbsjkfbnwekinbfkwnfdnbfwsjef</p>
+              <span>
+                  <div className = {clsx(styles.contentAlign)}>
+                      <p>sddikffbhsdjkdfvbbsjkfbnwekinbfkwnfdnbfwsjeffsdljfnsdmkjnfsdjnckseneknwnw</p>
+                      </div>
                   </span>
-              {/* <span className={clsx(styles.tickAlign)}>
-          
-            <Success />
-            </span> */}
+             
           
         </div>
     )
--- pages/report/index.tsx
+++ pages/report/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
-import { SimpleGrid }  from '@chakra-ui/react';
+import { ChakraProvider, SimpleGrid, CircularProgress, CircularProgressLabel, Center } from '@chakra-ui/react';
+
 
 import ReportCard from  '../../components/ReportCard';
 import Toggle from '../../components/Toggle';
@@ -8,12 +9,19 @@ import Toggle from '../../components/Toggle';
 import styles from './index.module.css';
 const index = () => {
     return (
+        <ChakraProvider>
         <div>
+            <Center className={styles.totalScore}>
+            <CircularProgress value = {50} thickness="15px" size="100px" color="#0edaff">
+                <CircularProgressLabel>50</CircularProgressLabel>
+            </CircularProgress>
+            </Center>
             <Toggle />
             <SimpleGrid  className={styles.grid} columns={2}  spacing={30}>
             <ReportCard />
             </SimpleGrid>
         </div>
+        </ChakraProvider>
     )
 }
 
