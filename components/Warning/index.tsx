795cf50fc6fa9006b842ff633980cd485f716685
Sarthak Kundra <sarthakkundra21@gmail.com>
2021-01-01 14:12:44 +0530

    MERGE AFTER HACKATHON

--- components/Warning/index.tsx
+++ components/Warning/index.tsx
@@ -5,24 +5,10 @@ import styles from "./index.module.css";
 const index = () => {
   return (
     <>
-      <svg
-        className={clsx(styles.checkmark)}
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 52 52"
-      >
-        <circle
-          className={clsx(styles.checkmark_circle)}
-          cx="26"
-          cy="26"
-          r="25"
-          fill="none"
-        />
-        <path
-          className={clsx(styles.checkmark_check)}
-          fill="none"
-          d="M14.1 27.2l7.1 7.2 16.7-16.8"
-        />
-      </svg>
+     <svg className={clsx(styles.checkmark)} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52">
+  <circle className={clsx(styles.checkmark__circle)} cx="26" cy="26" r="25" fill="none" />
+  <path className={clsx(styles.checkmark__check)} fill="none" d="M16 16 36 36 M36 16 16 36" />
+</svg>
     </>
   );
 };
