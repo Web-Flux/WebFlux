001ebb6e28fa94760af7deaa793d6add68395cc2
Karan Sheth <karan.sheth@somaiya.edu>
2021-01-22 23:02:02 +0530

    Static report for demo

--- context/SEO/SEOReducer.ts
+++ context/SEO/SEOReducer.ts
@@ -1,29 +1,27 @@
-import {
-    GET_REPORT,
-    SET_LOADING,
-    SET_URL
-} from '../types';
+import { GET_REPORT, SET_LOADING, SET_URL } from "../types";
 
-export default (state, action) => {
-    switch (action.type) {
-        case SET_LOADING:
-            return{
-                ...state,
-                loading: true
-            }
+const reducer = (state, action) => {
+  switch (action.type) {
+    case SET_LOADING:
+      return {
+        ...state,
+        loading: true,
+      };
 
-        case SET_URL:
-            return{
-                ...state,
-                url: action.payload
-            }
-        
-            case GET_REPORT: 
-                return{
-                    ...state,
-                    // TODO - Fill according to API docs
-                }
-        default:
-            return state;
-    }
-}
+    case SET_URL:
+      return {
+        ...state,
+        url: action.payload,
+      };
+
+    case GET_REPORT:
+      return {
+        ...state,
+        ...action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,26 @@
-import Head from 'next/head'
-import Link from 'next/link';
+import Head from "next/head";
+import Link from "next/link";
 
-import { ChakraProvider, CSSReset } from '@chakra-ui/react';
-import styles from '../styles/Home.module.css'
+import { ChakraProvider, CSSReset } from "@chakra-ui/react";
+import styles from "../styles/Home.module.css";
 
-import SEOState from '../context/SEO/SEOState';
-import HomePage from './HomePage';
+import SEOState from "../context/SEO/SEOState";
+import HomePage from "./HomePage";
 
 export default function Home() {
- 
-
   return (
-    
     <ChakraProvider>
       <SEOState>
-      <CSSReset />
-    <div className={styles.container}>
-      <Head>
-        <title>SEO Enhancer</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
- 
-      <HomePage />
-    </div>
-    </SEOState>
-    </ChakraProvider>
-  
+        <CSSReset />
+        <div className={styles.container}>
+          <Head>
+            <title>SEO Enhancer</title>
+            <link rel="icon" href="/favicon.ico" />
+          </Head>
 
-  )
+          <HomePage />
+        </div>
+      </SEOState>
+    </ChakraProvider>
+  );
 }
